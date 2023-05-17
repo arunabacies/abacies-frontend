1c8ea6ab9c488270e826c758bd046c710c222243
unknown <dhanya@abacies.com>
2023-05-17 16:50:45 +0530

    update

--- src/configs/apiConfig.js
+++ src/configs/apiConfig.js
@@ -3,8 +3,8 @@
 //Api config options
 const apiConfig = {
     api: {
-      // url:'https://sub-abacies.abacies.com/wp-admin/wp-json/'
-      url:'http://localhost/abacies/wp-json/'
+      url:'https://sub-abacies.abacies.com/wp-json/'
+      // url:'http://localhost/abacies/wp-json/'
     },
     // token: {
     //   // url2: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MjlmNjUwMWVmNGY2MzJkNDJmNDE0ZWIiLCJpYXQiOjE2NTQ2MTMyNjF9.JprEIgfAqT0wuLkcXxVqZ22TMmCz1s03ex2VlrnqFRw' // old token
